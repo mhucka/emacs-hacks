b3cc25871adcaec2d752d451b3d008a2787b32c7
Michael Hucka <mhucka@caltech.edu>
2024-01-22 18:55:25 -0800

    feat: start some makefile-mode snippets

---
+++ snippets/makefile-mode/section-heading.el
@@ -0,0 +1,6 @@
+# -*- mode: snippet -*-
+# name: section-heading
+# key: file
+# --
+# ${1:text} ${$$(when yas-moving-away-p (make-string (- 76 (string-width (yas-field-value 1))) (aref "~" 0)))}
+$0
