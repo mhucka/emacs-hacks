f0124c025f2300da8bf5482ca6989b531be04064
Michael Hucka <mhucka@caltech.edu>
2024-02-10 13:11:08 -0800

    feat: add a couple of "tell" snippets I use often

---
+++ snippets/applescript-mode/tell-dt.el
@@ -0,0 +1,8 @@
+# -*- mode: snippet; tab-width: 4 -*-
+# name: tell-dt
+# expand-env: ((yas-indent-line 'fixed))
+# key: file
+# --
+tell application id "DNtp"
+	$0
+end tell
---
+++ snippets/applescript-mode/tell-system.el
@@ -0,0 +1,8 @@
+# -*- mode: snippet; tab-width: 4 -*-
+# name: tell-system
+# expand-env: ((yas-indent-line 'fixed))
+# key: file
+# --
+tell application "System Events"
+	$0
+end tell
