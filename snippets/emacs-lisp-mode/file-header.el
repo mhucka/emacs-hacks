880f20606a0b153384f54607cc3a99ef78a6eaf8
Michael Hucka <mhucka@caltech.edu>
2024-02-10 13:13:55 -0800

    refactor: make this match my usual header style

--- snippets/emacs-lisp-mode/file-header.el
+++ snippets/emacs-lisp-mode/file-header.el
@@ -2,8 +2,8 @@
 # name: file-header
 # key: file
 # --
-;;;; @file    `(file-name-nondirectory (buffer-file-name))`
-;;;; @brief   `(read-string "File summary: ")`
-;;;; @author  Michael Hucka <mhucka@caltech.edu>
-;;;; @license MIT license; please see the file LICENSE in the repository
-;;;; @repo    https://github.com/`(let ((default-directory (file-name-directory (buffer-file-name)))) (shell-command-to-string "git config --get remote.origin.url | cut -f2 -d':' | cut -f1 -d'.' | tr -d '\n'"))`
+;;;; Summary: `(read-string "Summary: ")`.
+;;;;
+;;;; Copyright `(nth 5 (decode-time))` Michael Hucka.
+;;;; License: MIT license – see file "LICENSE" in the project website.
+;;;; Website: `(github-repo-url)`
